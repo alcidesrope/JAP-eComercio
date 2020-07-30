b2ec8dd87722c2823035f5685e8fcfe2aa1a7092
Alcides Rosano <accros_93@hotmail.com>
2020-07-30 10:51:02 -0300

    Console pamela

--- js/category-info.js
+++ js/category-info.js
@@ -44,6 +44,7 @@ document.addEventListener("DOMContentLoaded", function(e){
             console.log("Debajo de esta linea escriban su nombre: ");
             console.log("Agustin Tosar");
             console.log("Alcides Rosano");
+            console.log("Pamela Regalados");
         }
     });
 });
